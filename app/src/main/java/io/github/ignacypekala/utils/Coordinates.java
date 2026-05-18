85e201e07d95ae9e93540bb56d923d8c90a5d4a0
Ignacy Pękała <ignacy@pekala.eu>
2026-05-18 09:23:41 +0200

    Rename coordinates getters

--- app/src/main/java/io/github/ignacypekala/utils/Coordinates.java
+++ app/src/main/java/io/github/ignacypekala/utils/Coordinates.java
@@ -9,11 +9,11 @@ public class Coordinates {
         this.y = y;
     }
 
-    public int x() {
+    public int getX() {
         return x;
     }
 
-    public int y() {
+    public int getY() {
         return y;
     }
 }
