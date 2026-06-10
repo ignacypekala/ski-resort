b0ac26f0c6c11aec4eef41824fa0847231f31d36
Ignacy Pękała <ignacy@pekala.eu>
2026-06-10 12:57:38 +0200

    Add a simple LiftQueue test

--- app/src/test/java/io/github/ignacypekala/lift/LiftQueueTest.java
+++ app/src/test/java/io/github/ignacypekala/lift/LiftQueueTest.java
@@ -13,6 +13,39 @@ public class LiftQueueTest {
     }
 
     @Test
+    void simple() {
+        LiftQueue queue = new LiftQueue();
+        Skier[] skiers = new Skier[5];
+        for (int i = 0; i < 5; i++) {
+            skiers[i] = new TestClass.TestSkier(i, 0, 0, 0);
+        }
+
+        assertTrue(queue.empty());
+        queue.enqueue(skiers[0]);
+        assertFalse(queue.empty());
+        queue.enqueue(skiers[1]);
+
+        assertSame(skiers[0], queue.peek());
+        assertDoesNotThrow(() -> queue.dequeue());
+        assertFalse(queue.empty());
+
+        queue.enqueue(skiers[2]);
+        queue.enqueue(skiers[3]);
+        queue.enqueue(skiers[4]);
+
+        assertSame(skiers[1], queue.peek());
+        assertDoesNotThrow(() -> queue.dequeue());
+
+        assertSame(skiers[2], queue.peek());
+        assertDoesNotThrow(() -> queue.dequeue());
+
+        assertFalse(queue.empty());
+        assertDoesNotThrow(() -> queue.dequeue());
+        assertDoesNotThrow(() -> queue.dequeue());
+        assertTrue(queue.empty());
+    }
+
+    @Test
     void arbitrary() {
         LiftQueue queue = new LiftQueue();
 
